f4a52f845b4d299ab2bcbea700a6e188d1d67e37
Matthew Thompson <matthew.thompson.a@gmail.com>
2016-08-26 13:32:26 -0400

    Created Inherited Class MapLocation : Point

--- TowerObjects/TowerObjects/Game.cs
+++ TowerObjects/TowerObjects/Game.cs
@@ -5,20 +5,60 @@ namespace TowerDefense {
         public static void Main(){
             Map firstMap = new Map(8, 5);
 
-            int area = firstMap.Width * firstMap.Height;
-            System.Console.WriteLine(area);
-
-            Point point = new Point(4, 2);       
-            bool isOnMap = firstMap.OnMap(point);
-            Console.WriteLine(isOnMap);
-
-
-            point = new Point(8, 5);
-            isOnMap = firstMap.OnMap(point);
-            Console.WriteLine(isOnMap);
-
-            point = new Point(4, 2);
-            Console.WriteLine(point.DistanceTo(5, 5)); 
+            try
+            {
+                MapLocation mapLocation = new MapLocation(20, 20, firstMap);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("That map location is not on the map."); 
+            }
         }
     }
+
+
+
+    /*
+
+# int area = firstMap.Width * firstMap.Height;
+# System.Console.WriteLine(area);
+#
+# Point point = new Point(4, 2);
+# bool isOnMap = firstMap.OnMap(point);
+# Console.WriteLine(isOnMap);
+#
+#
+# point = new Point(8, 5);
+# isOnMap = firstMap.OnMap(point);
+# Console.WriteLine(isOnMap);
+#
+# point = new Point(4, 2);
+# Console.WriteLine(point.DistanceTo(5, 5));
+ 
+    Inheritance Example 
+     
+    class Polygon
+    {
+        public readonly int NumSides;
+
+        public Polygon(int numSides)
+        {
+            NumSides = numSides;
+        }
+    }
+
+        class Square : Polygon 
+        {
+            public readonly int SideLength; 
+            public Square(int sideLength) : base(4) 
+            {
+                SideLength = sideLength; 
+            }
+        }
+
+     */
+
+
+
+
 }
---
+++ TowerObjects/TowerObjects/MapLocation.cs
@@ -0,0 +1,13 @@
+﻿namespace TowerDefense
+{
+    class MapLocation : Point
+    {
+        public MapLocation(int x, int y, Map firstMap) : base(x, y)
+        {
+            if (!firstMap.OnMap(this)) //if this point is not on firstMap 
+            {
+                throw new System.Exception(); 
+            }
+        }
+    }
+}
