695bf97da89b6548932d9207f9fdb37c64b034b0
Matthew Thompson <matthew.thompson.a@gmail.com>
2016-08-30 16:06:41 -0400

    Created Accuracy Random Number generator for Towers. Game runs in Console.

--- TowerObjects/TowerObjects/Tower.cs
+++ TowerObjects/TowerObjects/Tower.cs
@@ -3,13 +3,22 @@
     class Tower
     {
         private const int _range = 1;
-        private const int _power = 1; 
+        private const int _power = 1;
+        private const double _accuracy = .75; //75% hit rate 
+ 
+        //for tower miss 
+        private static readonly System.Random _random = new System.Random(); 
         
         private readonly MapLocation _location;
 
         public Tower(MapLocation location)
         {
             _location = location; 
+        }
+
+        public bool IsSuccessfulShot()
+        {
+            return _random.NextDouble() < _accuracy; 
         }
 
         public void FireOnInvaders(Invader[] invaders)
@@ -19,8 +28,23 @@
                 //Do Stuff 
                 if (invader.IsActive && _location.InRangeOf(invader.Location, _range))
                 {
-                    invader.DecreaseHealth(_power); 
+                    if (IsSuccessfulShot())
+                    {
+                        invader.DecreaseHealth(_power);
+                        System.Console.WriteLine("Shot and hit an invader!");
+
+                        if (invader.IsNeutralized)
+                        {
+                            System.Console.WriteLine("He GONE!");
+                        }
+                    }
+                    else
+                    {
+                        System.Console.WriteLine("Missed... "); 
+                    }
+
                     break;
+
                 }
             }
         } 
