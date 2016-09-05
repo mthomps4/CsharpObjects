8a4e993599eddd1a01a1aab87ef4d5ccd09922fc
Matt Thompson <matthew.thompson.a@gmail.com>
2016-09-05 19:00:18 -0400

    Converted Tower Class to Virtual Properties

--- TowerObjects/TowerObjects/Tower.cs
+++ TowerObjects/TowerObjects/Tower.cs
@@ -2,9 +2,9 @@
 {
     class Tower
     {
-        private const int _range = 1;
-        private const int _power = 1;
-        private const double _accuracy = .75; //75% hit rate 
+        protected virtual int Range { get; } = 1;
+        protected virtual int Power{get;} = 1;
+        protected virtual double Accuracy{get;} = .75; //75% hit rate 
  
         //for tower miss 
         private static readonly System.Random _random = new System.Random(); 
@@ -18,7 +18,7 @@
 
         public bool IsSuccessfulShot()
         {
-            return _random.NextDouble() < _accuracy; 
+            return _random.NextDouble() < Accuracy; 
         }
 
         public void FireOnInvaders(Invader[] invaders)
@@ -26,11 +26,11 @@
             foreach (Invader invader in invaders)
             {
                 //Do Stuff 
-                if (invader.IsActive && _location.InRangeOf(invader.Location, _range))
+                if (invader.IsActive && _location.InRangeOf(invader.Location, Range))
                 {
                     if (IsSuccessfulShot())
                     {
-                        invader.DecreaseHealth(_power);
+                        invader.DecreaseHealth(Power);
                         if (invader.IsNeutralized)
                         {
                             System.Console.WriteLine("He GONE!");
